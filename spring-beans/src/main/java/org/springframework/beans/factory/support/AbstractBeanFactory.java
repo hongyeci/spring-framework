768cd2e9e6a60d1c00d3c4012bab4ba648cb2d09
hongyeci <1793172336@qq.com>
2023-08-27 13:39:03 +0800

    spring 源码编译问题解决

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -234,6 +234,7 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
 	}
 
 	/**
+	 * 返回指定bean的一个实例，该实例可以是共享的，也可以是独立的
 	 * Return an instance, which may be shared or independent, of the specified bean.
 	 * @param name the name of the bean to retrieve
 	 * @param requiredType the required type of the bean to retrieve
--- spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -74,6 +74,7 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 	private static final int SUPPRESSED_EXCEPTIONS_LIMIT = 100;
 
 
+	//单例池
 	/** Cache of singleton objects: bean name to bean instance. */
 	private final Map<String, Object> singletonObjects = new ConcurrentHashMap<>(256);
 
